702e32fbfbfa9e094e718ff32e164b6e7913e970
Nikita Volynkin <niknorff@gmail.com>
2017-04-10 11:36:24 +0300

    Frontend listen to heroku

--- chat-frontend.js
+++ chat-frontend.js
@@ -24,7 +24,7 @@ $(function () {
     }
 
     // open connection
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    var connection = new WebSocket('ws://chat-ionic.herokuapp.com/');
 
     connection.onopen = function () {
         // first we want users to enter their names
